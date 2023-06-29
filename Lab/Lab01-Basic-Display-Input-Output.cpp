cd172f0a4e65fb048f3682ae9e22b5f233f98d3e
RubyQWER <Chanatpong1234@gmail.com>
2023-06-29 14:21:46 +0700

    11

--- Lab/Lab01-Basic-Display-Input-Output.cpp
+++ Lab/Lab01-Basic-Display-Input-Output.cpp
@@ -6,10 +6,9 @@ int main() {
     printf( "Enter your name: " );
     scanf( "%s", Name );
     printf( "Enter your age: " );
-    scanf("%d", &Age) ;
+    scanf("%d", &Age);
     printf( "- - - - - -\n" ) ;
     printf( "Hello = %s\n", Name ) ; //TODO: #14 Say hello to user.
-    
     printf( "Age = %d\n", Age ) ; //TODO: #15 display user their age number plus 1.
     return 0 ;
 }//end main function
